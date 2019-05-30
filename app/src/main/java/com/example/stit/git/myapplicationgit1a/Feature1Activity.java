2050ba7087f6a8d11d7161af23c9e7b5c84ef019
stit <stit@gmail.com>
2019-05-30 11:10:30 +0800

    #finished added feature 1 activity

---
+++ app/src/main/java/com/example/stit/git/myapplicationgit1a/Feature1Activity.java
@@ -0,0 +1,13 @@
+package com.example.stit.git.myapplicationgit1a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+ß∑
+public class Feature1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feature1);
+    }
+}
