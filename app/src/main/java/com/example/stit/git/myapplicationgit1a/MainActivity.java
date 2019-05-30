d6576a0e033896269bb9b03b26dfd99cc5e8fba1
stit <stit@gmail.com>
2019-05-30 11:06:46 +0800

    #finished added textview

--- app/src/main/java/com/example/stit/git/myapplicationgit1a/MainActivity.java
+++ app/src/main/java/com/example/stit/git/myapplicationgit1a/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.stit.git.myapplicationgit1a;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
