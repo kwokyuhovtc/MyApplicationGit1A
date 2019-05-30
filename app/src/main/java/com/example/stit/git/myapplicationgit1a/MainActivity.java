271c5eae335a04cafdd734ab0a14b45e72bd1b18
stit <stit@gmail.com>
2019-05-30 10:59:28 +0800

    #finished added button in main activity.java
    #todo
    add button in xml layout file

--- app/src/main/java/com/example/stit/git/myapplicationgit1a/MainActivity.java
+++ app/src/main/java/com/example/stit/git/myapplicationgit1a/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.stit.git.myapplicationgit1a;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
